5e217420e61a277f8a37ef9b511fb5c05847c796
rafli4514 <muhammadrafli.tkn@gmail.com>
2025-01-18 01:18:54 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+
+  return (
+    <div>
+      <header>
+        <img src="/chat-bot.png" alt="chatbot" />
+        <h2>AI Chatbot</h2>
+      </header>
+      <div />
+    </div>
+  )
+}
+
+export default App
