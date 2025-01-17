fdf11f8728acec29580804ee842694c17f2f9654
rafli4514 <muhammadrafli.tkn@gmail.com>
2025-01-18 03:10:47 +0700

    Build UI Chatbot

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
+import { useState } from "react"
+import { Chat } from "./components/Chat/Chat"
+import { Controls } from "./components/Controls/Controls"
+import styles from "./App.module.css"
 
 function App() {
+  const [messages, setMessages] = useState([])
+
+  function handleContentSend(content){
+    setMessages((prevMessages) => [...prevMessages, { content, role: "user" }]);
+  }
 
   return (
-    <div>
-      <header>
-        <img src="/chat-bot.png" alt="chatbot" />
-        <h2>AI Chatbot</h2>
+    <div className={styles.App}>
+      <header className={styles.Header}>
+        <img className={styles.Logo} src="/chat-bot.png" alt="chatbot" />
+        <h2 className={styles.Title}>AI Chatbot</h2>
       </header>
-      <div />
+      <div className={styles.ChatContainer}>
+        <Chat messages={messages} />
+      </div>
+      <Controls onSend={handleContentSend} />
     </div>
   )
 }
---
+++ src/components/Chat/Chat.jsx
@@ -0,0 +1,19 @@
+import styles from './Chat.module.css'
+
+const WELCOME_MESSAGE = {
+    role: 'assistant',
+    content: 'Halo! Bagaimana saya membantu kamu sekarang?'
+}
+
+export function Chat({ messages }) {
+    return (
+    <div className={styles.Chat}>
+        {[WELCOME_MESSAGE, ...messages].map(({ role, content }, index) => (
+            <div key={index} className={styles.Message} data-role={role}>
+                {content} 
+            </div>
+        ))}
+    </div>
+    )
+}
+
