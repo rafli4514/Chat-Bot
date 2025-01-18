2ec0c4232b214cea5f9701ae589f035905baca4e
rafli4514 <muhammadrafli.tkn@gmail.com>
2025-01-18 20:42:22 +0700

    auto scrolling

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,29 +1,45 @@
 import Markdown from 'react-markdown'
 import styles from './Chat.module.css'
-import { useRef, useEffect } from 'react'
+import { useRef, useEffect, useMemo } from 'react'
 
 
-const WELCOME_MESSAGE = {
-    role: 'assistant',
-    content: 'Halo! Bagaimana saya membantu kamu sekarang?'
-}
+const WELCOME_MESSAGE_GROUP = [
+    {
+        role: 'assistant',
+        content: 'Halo! Bagaimana saya membantu kamu sekarang?'
+    }
+]
 
 export function Chat({ messages }) {
     const messagesEndRef = useRef(null)
+    const messagesGroups = useMemo(() => messages.reduce((groups, message) => {
+        if (message.role === 'user') groups.push([])
+        groups[groups.length - 1].push(message)
+        return groups
+    }, []), [messages])
 
-    useEffect(() =>{
+    useEffect(() => {
         messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' })
     }, [messages])
 
     return (
-    <div className={styles.Chat}>
-        {[WELCOME_MESSAGE, ...messages].map(({ role, content }, index) => (
-            <div key={index} className={styles.Message} data-role={role}>
-                <Markdown>{content}</Markdown>
-            </div>
-        ))}
-        <div ref={messagesEndRef} />
-    </div>
+        <div className={styles.Chat}>
+            {[WELCOME_MESSAGE_GROUP, ...messagesGroups].map(
+                (messages, groupIndex) => (
+                    // Group
+                    <div key={groupIndex} className={styles.Group}>
+                        {messages.map(({ role, content }, index) => (
+                            // Message
+                            <div key={index} className={styles.Message} data-role={role}>
+                                <Markdown>{content}</Markdown>
+                            </div>
+                        ))}
+                    </div>
+                )
+            )}
+
+            <div ref={messagesEndRef} />
+        </div>
     )
 }
 
